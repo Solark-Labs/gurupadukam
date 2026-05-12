951720f6e25c9f335442727b16db48424f74c806
Go-solark <271959710+Solark-Labs@users.noreply.github.com>
2026-05-12 21:59:45 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,27 +1,13 @@
-import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
-  return {
-    plugins: [react()],
-    base: "/gurupadukam/",
-    server: {
-      proxy: {
-        // Target your Node.js backend (local dev only)
-        '/api-proxy': 'http://localhost:5000',
-        '/ws-proxy': { target: 'ws://localhost:5000', ws: true },
-      },
+export default defineConfig({
+  plugins: [react()],
+  base: "/", 
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-    define: {
-      // Example env variable (not actually used in production)
-      'process.env.API_KEY': JSON.stringify('api-key-this-is-not-used-can-be-ignored!'),
-    },
-  };
+  },
 });
