390e49e0afd71fe458814b9b3ea853280c3e5e39
Go-solark <271959710+Solark-Labs@users.noreply.github.com>
2026-05-15 00:29:49 +0530

    Update vite.config.ts

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,17 +1,3 @@
-#import { defineConfig } from 'vite';
-#import react from '@vitejs/plugin-react';
-#import path from 'path';
-
-#export default defineConfig({
- # plugins: [react()],
- # base: "/", 
-  #resolve: {
-   # alias: {
-    #  '@': path.resolve(__dirname, '.'),
-    #},
-  #},
-#});
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
